2a1738da64a165d6541c9cfb5bf8d6ccea7cc518
Linoy Malol <linoymalol1@gmail.com>
2026-01-10 00:30:22 +0200

    chore: update demo config

--- samples/RateLimiter.Demo/Program.cs
+++ samples/RateLimiter.Demo/Program.cs
@@ -1,7 +1,18 @@
 using RateLimiter.Core;
 using RateLimiter.Rules;
+using RateLimiter.Models;
 
-var limiter = new InMemoryRateLimiter(new RateLimiterOptions());
+var options = new RateLimiterOptions
+{
+    DefaultRule = new RouteRule(60, TimeSpan.FromMinutes(1)),
+    RouteOverrides = new Dictionary<string, RouteRule>(StringComparer.OrdinalIgnoreCase)
+    {
+        ["/login"] = new RouteRule(10, TimeSpan.FromSeconds(10))
+    }
+};
+
+var limiter = new InMemoryRateLimiter(options);
+
 
 for (int i = 1; i <= 65; i++)
 {
